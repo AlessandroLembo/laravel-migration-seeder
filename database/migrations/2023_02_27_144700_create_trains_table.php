0497e8b7730f58b7f6aa28eb8f1d591757252cae
AlessandroLembo <alessandrolembo2@gmail.com>
2023-02-27 16:09:16 +0100

    create table structure

--- database/migrations/2023_02_27_144700_create_trains_table.php
+++ database/migrations/2023_02_27_144700_create_trains_table.php
@@ -13,7 +13,16 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-
+            $table->string('Azienda', 30);
+            $table->string('Stazione di partenza', 50);
+            $table->string('Stazione di arrivo', 50);
+            $table->string('Data', 12);
+            $table->float('Orario di partenza', 4, 2);
+            $table->float('Orario di arrivo', 4, 2);
+            $table->string('Codice treno', 40);
+            $table->tinyInteger('Numero carrozze')->unsigned();
+            $table->boolean('In orario')->default(1);
+            $table->boolean('Cancellato')->default(0);
             $table->timestamps();
         });
     }
